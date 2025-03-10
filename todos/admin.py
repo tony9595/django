8de1e3c8ecfe2bb155ab54fb95345f48a62dea2d
youngjeaMo <tony9595@naver.com>
2025-03-10 14:14:36 +0900

    장고 프로젝트에 Todo 모델 및 관련 뷰, 폼 추가; 설정 파일 및 템플릿 업데이트

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from todos.models import Todo
+
 # Register your models here.
+# dev_2
+admin.site.register(Todo)
--- todos/urls.py
+++ todos/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 from todos import views
+
+
 
 # dev_1
 urlpatterns = [
-    path("", views.home,name="home"),
+    path("", views.todo_list, name="todo_list"),  # dev_3
+    path("post/", views.todo_post, name="todo_post"),  # dev_3
 ]
